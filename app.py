e0f4db10faa3f35299cf9f72531e9073a8a0a7d7
tharunoptimus <tharunoptimus@outlook.com>
2023-07-29 16:59:44 +0530

    Added function that converts base64 to png

--- app.py
+++ app.py
@@ -16,6 +16,22 @@ def hello_world():
 	return "PONG"
 
 
+def base64_to_png(base64_data, output_path):
+    try:
+        # Decode the base64 data into bytes
+        png_bytes = base64.b64decode(base64_data)
+
+        # Create a PIL Image object from the bytes data
+        image = Image.open(io.BytesIO(png_bytes))
+
+        # Save the image to the specified output path
+        image.save(output_path, format="PNG")
+        return output_path
+
+    except Exception as e:
+        print("Error:", e)
+        return None
+
 
 if __name__ == '__main__':
 	app.run()
